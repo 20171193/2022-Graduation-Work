71189ff7ae3d88db802eb21408d2db85062a26c8
Euncheol Shin <dmscjf3819@gmail.com>
2022-02-11 09:26:00 +0900

    ReTest
    처음만든 프로젝트가 버그걸려서 다시만듬

--- Gradu_ECS/Source/Gradu_ECS/Gradu_ECS.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Gradu_ECS.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Gradu_ECS, "Gradu_ECS" );
- 
