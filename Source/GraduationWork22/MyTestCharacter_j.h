42bfb15c8cbd527d1557a782adcff6a89e3869ca
Shim Jae Chun <shimnaldo98@gmail.com>
2022-02-26 16:44:18 +0900

    02.26 UI 및 애니메이션
    * 작업 내용
    데이터 테이블 생성 : 캐릭터 관련 (이름, 레벨, 이미지 등)
    애니메이션 추가 : 궁수 캐릭터 (+ 관련 애니메이션)
    * 작업 할 내용
    애니메이션 트리 생성.
    애니메이션 구현.

--- Source/GraduationWork22/MyTestCharacter_j.h
+++ Source/GraduationWork22/MyTestCharacter_j.h
@@ -4,16 +4,45 @@
 
 #include "CoreMinimal.h"
 #include "BasicCharacter_j.h"
+#include "Engine/DataTable.h"
 #include "MyTestCharacter_j.generated.h"
 
 /**
  * 
  */
+USTRUCT(BlueprintType)
+struct FCharacterInfo : public FTableRowBase
+{
+	GENERATED_BODY()
+public:
+	FCharacterInfo()
+	{
+		CharacterName = FText::FromString("Name");
+		CharacterLevel = 1;
+		Description = FText::FromString("Your Character is");
+	}
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FName CharacterID;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText CharacterName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UTexture2D* CharacterThumbnall;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 CharacterLevel;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText Description;
+	
+};
+
 UCLASS()
 class GRADUATIONWORK22_API AMyTestCharacter_j : public ABasicCharacter_j
 {
 	GENERATED_BODY()
-	
 public:
 	AMyTestCharacter_j();
 
