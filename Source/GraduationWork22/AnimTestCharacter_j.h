6bae8bb33c49b1f0a5b8ea17ceb0ee598b6901ca
Shim Jae Chun <shimnaldo98@gmail.com>
2022-04-19 02:58:39 +0900

    0419 사운드적용 오류 수정

--- Source/GraduationWork22/AnimTestCharacter_j.h
+++ Source/GraduationWork22/AnimTestCharacter_j.h
@@ -41,6 +41,9 @@ public:
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
 	class UCameraComponent* CameraComponent;
 
+	UPROPERTY(EditDefaultsOnly)
+	class USoundBase* SB_Jump;
+
 public:
 
 	UFUNCTION()
