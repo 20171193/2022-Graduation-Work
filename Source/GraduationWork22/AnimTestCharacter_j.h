0e1a2777858b5aa25877c8d99b25fa38c02fd6ab
Shim Jae Chun <shimnaldo98@gmail.com>
2022-06-06 21:51:18 +0900

    0606 작업분

--- Source/GraduationWork22/AnimTestCharacter_j.h
+++ Source/GraduationWork22/AnimTestCharacter_j.h
@@ -35,7 +35,7 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	UPROPERTY(VisibleAnywhere)
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
 	class USpringArmComponent* CameraSpringArmComponent;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
